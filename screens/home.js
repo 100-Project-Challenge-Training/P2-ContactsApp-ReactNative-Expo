a3e63a0a3615c3384f90a2ee5f1976503eaa2d13
abeeral-rafati <abeerw.rafati@yahoo.com>
2021-08-27 16:43:24 +0300

    test3

--- screens/home.js
+++ screens/home.js
@@ -1,51 +1,114 @@
 import React from "react";
-import { StyleSheet, Text, View, Image } from "react-native";
-import { Card } from "react-native-paper";
+import { StyleSheet, Text, View, Image, FlatList } from "react-native";
+import { Card, FAB } from "react-native-paper";
+import { Colors } from "react-native/Libraries/NewAppScreen";
 
 function Home() {
   const data = [
     {
-      id: 1,
+      id: "1",
       name: "Abeer W.Rafati",
       job: "React-Native Developer",
       img: "https://thispersondoesnotexist.com/image",
     },
     {
-      id: 2,
+      id: "2",
       name: "Esraa W.Rafati",
       job: "Web Developer",
       img: "https://thispersondoesnotexist.com/image",
     },
     {
-      id: 3,
+      id: "3",
       name: "Mohammed W.Rafati",
       job: "Full-Stack Developer",
       img: "https://thispersondoesnotexist.com/image",
     },
     {
-      id: 4,
+      id: "4",
+      name: "Maram W.Rafati",
+      job: "React Developer",
+      img: "https://thispersondoesnotexist.com/image",
+    },
+    {
+      id: "5",
+      name: "Maram W.Rafati",
+      job: "React Developer",
+      img: "https://thispersondoesnotexist.com/image",
+    },
+    {
+      id: "6",
+      name: "Maram W.Rafati",
+      job: "React Developer",
+      img: "https://thispersondoesnotexist.com/image",
+    },
+    {
+      id: "7",
+      name: "Maram W.Rafati",
+      job: "React Developer",
+      img: "https://thispersondoesnotexist.com/image",
+    },
+    {
+      id: "8",
+      name: "Maram W.Rafati",
+      job: "React Developer",
+      img: "https://thispersondoesnotexist.com/image",
+    },
+    {
+      id: "9",
+      name: "Maram W.Rafati",
+      job: "React Developer",
+      img: "https://thispersondoesnotexist.com/image",
+    },
+    {
+      id: "10",
+      name: "Maram W.Rafati",
+      job: "React Developer",
+      img: "https://thispersondoesnotexist.com/image",
+    },
+    {
+      id: "11",
+      name: "Maram W.Rafati",
+      job: "React Developer",
+      img: "https://thispersondoesnotexist.com/image",
+    },
+    {
+      id: "12",
       name: "Maram W.Rafati",
       job: "React Developer",
       img: "https://thispersondoesnotexist.com/image",
     },
   ];
 
-  return data.map((employee) => {
-    return (
-      <Card style={styles.myCard} key={employee.id}>
-        <View style={styles.cardView}>
-          <Image
-            style={{ width: 60, height: 60, borderRadius: 30 }}
-            source={{ uri: employee.img }}
-          />
-          <View style={styles.textView}>
-            <Text style={styles.text}>{employee.name}</Text>
-            <Text>{employee.job}</Text>
-          </View>
-        </View>
-      </Card>
-    );
-  });
+  return (
+    <View>
+      <FlatList
+        data={data}
+        renderItem={({ item }) => {
+          return (
+            <Card style={styles.myCard} key={item.id}>
+              <View style={styles.cardView}>
+                <Image
+                  style={{ width: 60, height: 60, borderRadius: 30 }}
+                  source={{ uri: item.img }}
+                />
+                <View style={styles.textView}>
+                  <Text style={styles.text}>{item.name}</Text>
+                  <Text>{item.job}</Text>
+                </View>
+              </View>
+            </Card>
+          );
+        }}
+      />
+      <FAB
+        style={styles.fab}
+        small={false}
+        theme={{colors: {accent:'#006aff'}}}
+        icon="plus"
+        onPress={() => console.log("Pressed")}
+      />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
@@ -61,6 +124,12 @@ const styles = StyleSheet.create({
   },
   text: {
     fontSize: 24,
+  },
+  fab: {
+    position: "absolute",
+    margin: 16,
+    right: 0,
+    bottom: 0,
   },
 });
 export default Home;
