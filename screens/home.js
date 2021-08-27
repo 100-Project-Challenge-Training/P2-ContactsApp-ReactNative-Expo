07f82b3f035923aa67ac122dfe32739051903bdd
abeeral-rafati <abeerw.rafati@yahoo.com>
2021-08-27 16:09:49 +0300

    test2

--- screens/home.js
+++ screens/home.js
@@ -1,20 +1,66 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Image } from "react-native";
 import { Card } from "react-native-paper";
 
 function Home() {
-  return (
-    <Card style={styles.myCard}>
-      <Text style={{ fontSize: 24 }}>Hello from Home page</Text>
-      {/* <Text>hello again</Text> */}
-    </Card>
-  );
+  const data = [
+    {
+      id: 1,
+      name: "Abeer W.Rafati",
+      job: "React-Native Developer",
+      img: "https://thispersondoesnotexist.com/image",
+    },
+    {
+      id: 2,
+      name: "Esraa W.Rafati",
+      job: "Web Developer",
+      img: "https://thispersondoesnotexist.com/image",
+    },
+    {
+      id: 3,
+      name: "Mohammed W.Rafati",
+      job: "Full-Stack Developer",
+      img: "https://thispersondoesnotexist.com/image",
+    },
+    {
+      id: 4,
+      name: "Maram W.Rafati",
+      job: "React Developer",
+      img: "https://thispersondoesnotexist.com/image",
+    },
+  ];
+
+  return data.map((employee) => {
+    return (
+      <Card style={styles.myCard} key={employee.id}>
+        <View style={styles.cardView}>
+          <Image
+            style={{ width: 60, height: 60, borderRadius: 30 }}
+            source={{ uri: employee.img }}
+          />
+          <View style={styles.textView}>
+            <Text style={styles.text}>{employee.name}</Text>
+            <Text>{employee.job}</Text>
+          </View>
+        </View>
+      </Card>
+    );
+  });
 }
 
 const styles = StyleSheet.create({
   myCard: {
     margin: 5,
-    padding: 5,
+  },
+  cardView: {
+    flexDirection: "row",
+    padding: 6,
+  },
+  textView: {
+    marginLeft: 20,
+  },
+  text: {
+    fontSize: 24,
   },
 });
 export default Home;
